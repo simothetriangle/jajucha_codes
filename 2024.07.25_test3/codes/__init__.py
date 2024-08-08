0732b69947480f0c1e9f0d7b20e57d238dc663eb
simothetriangle <122960548+simothetriangle@users.noreply.github.com>
2024-08-08 12:24:10 +0900

    Update __init__.py

--- 2024.07.25_test3/codes/__init__.py
+++ 2024.07.25_test3/codes/__init__.py
@@ -1,4 +1,4 @@
- #자주차 기본 동작코드
+#자주차 기본 동작코드
 from control import Jajucha
 import cv2
 
@@ -10,6 +10,7 @@ def Fevt_1(distance): #evt_1 - scan obstacles
             return 0
 
 def Fevt_2(V, L, R): #evt_2 - line tracing
+    #returns 1 - 30, 30 / 2 - 60, 60
     #exception1_RoverL or LoverR -> chk V -> chk LR
     sV_e2 = 0
     eV_e2 = 0
@@ -53,12 +54,12 @@ def Fevt_2(V, L, R): #evt_2 - line tracing
     for ni in range(3):
         for nj in range(4):
             hV_e2 = 0
-            cntlim_Min = nj*43
-            cntlim_Max = cntlim_Min + 42
+            Vcntlim_Min = nj*43
+            Vcntlim_Max = cntlim_Min + 42
 
-            if(V[ni] >= cntlim_Min && V[ni] < cntlim_Max):
+            if(V[ni] >= Vcntlim_Min && V[ni] < Vcntlim_Max):
                 hV_e2 += 1
-            if(V[6-ni] >= cntlim_Min && V[6-ni] < cntlim_Max):
+            if(V[6-ni] >= Vcntlim_Min && V[6-ni] < Vcntlim_Max):
                 hV_e2 -= 1
 
             if(hEx1_e2 == 1):
@@ -66,30 +67,22 @@ def Fevt_2(V, L, R): #evt_2 - line tracing
             elif(hEx1_e2 == -1):
                 return 1
 
-    #chk LR <-- 수정 필요 (jajucha.control -> return, 각각 수치 변경)
-    if(L[2] == R[2]):
-            if(L[2]  < 320):
-                return 1
-            else:
-                return 2
-        elif(L[2] != R[2] and (L[2] < 160 or R[2] < 160)):
-            if(L[2] - R[2] < 0):
-                return 2
-            else:
-                return 1
-        elif(L[1] != R[1] and (L[1] < 160 or R[1] < 160)):
-            if(L[1] - R[1] < 0):
-                return 2
-            else:
-                return 1
-        elif(L[0] != R[0] and (L[0] < 160 or R[0] < 160)):
-            if(L[0] - R[0] < 0):
-                return 2
-            else:
-                return 1
-        else:
-            return 0
+    #chk LR
+    for ni in range(3):
+        for nj in range(4):
+            hLR_e2 = 0
+            LRcntlim_Min = nj*80
+            LRcntlim_Max = LRcntlim_Min + 79
 
+            if(L[2-ni] >= LRcntlim_Min && L[2-ni] < LRcntlim_Max):
+                hLR_e2 += 1
+            if(R[2-ni] >= LRcntlim_Min && R[2-ni] < LRcntlim_Max):
+                hLR_e2 -= 1
+            
+         if(hEx1_e2 == 1):
+             return 2
+         elif(hEx1_e2 == -1):
+             return 1
 
 def main_loop():
     #set variables
