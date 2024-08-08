a9a51c163030132c2b06e086300b8930c2a846e2
simothetriangle <122960548+simothetriangle@users.noreply.github.com>
2024-08-08 12:05:46 +0900

    Update __init__.py

--- 2024.07.25_test3/codes/__init__.py
+++ 2024.07.25_test3/codes/__init__.py
@@ -69,26 +69,26 @@ def Fevt_2(V, L, R): #evt_2 - line tracing
     #chk LR <-- 수정 필요 (jajucha.control -> return, 각각 수치 변경)
     if(L[2] == R[2]):
             if(L[2]  < 320):
-                jajucha.control(30,30,speedTurn)
+                return 1
             else:
-                jajucha.control(60,60,speedTurn)
+                return 2
         elif(L[2] != R[2] and (L[2] < 160 or R[2] < 160)):
             if(L[2] - R[2] < 0):
-                jajucha.control(60,60,speedTurn)
+                return 2
             else:
-                jajucha.control(30,30,speedTurn)
+                return 1
         elif(L[1] != R[1] and (L[1] < 160 or R[1] < 160)):
             if(L[1] - R[1] < 0):
-                jajucha.control(60,60,speedTurn)
+                return 2
             else:
-                jajucha.control(30,30,speedTurn)
+                return 1
         elif(L[0] != R[0] and (L[0] < 160 or R[0] < 160)):
             if(L[0] - R[0] < 0):
-                jajucha.control(60,60,speedTurn)
+                return 2
             else:
-                jajucha.control(30,30,speedTurn)
+                return 1
         else:
-            jajucha.control(45,45,speedOrig)
+            return 0
 
 
 def main_loop():
